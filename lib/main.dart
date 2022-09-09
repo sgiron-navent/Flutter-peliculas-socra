b68ace42ca07ecafa5ed69d6e259c6bb39f64a0a
Sergio Giron <sgiron@navent.com>
2022-09-09 09:40:32 -0300

    .....

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
+import 'package:provider/provider.dart';
 
-import 'package:peliculas/src/pages/home_page.dart';
-import 'package:peliculas/src/pages/pelicula_detalle.dart';
+import 'package:peliculas/providers/movies_provider.dart';
  
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
- 
+void main() => runApp(AppState());
+
+
+class AppState extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => MoviesProvider(), lazy: false ),
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
+
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Películas',
-      initialRoute: '/',
+      initialRoute: 'home',
       routes: {
-        '/'       : ( BuildContext context ) => HomePage(),
-        'detalle' : ( BuildContext context ) => PeliculaDetalle(),
+        'home':    ( _ ) => HomeScreen(),
+        'details': ( _ ) => DetailsScreen(),
       },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(
+          color: Colors.indigo
+        )
+      ),
     );
   }
 }
